5520a9cbb15598e66b63f7357ff41593ad4f37c3
Bennyz2030 <nicholasbenzinger@gmail.com>
2022-03-30 14:05:09 -0700

    finish js business logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,118 @@
-function translate(string) {
-  let splitStr = string.toLowerCase().split(" ");
-  let pigArray = [];
-  for (i=0; i<splitStr.length; i++) {
-    pigArray.push((splitStr.slice(0,1).match(/[aeiou]/gi) !== null))
+// function translate(string) {
+//   let splitStr = string.toLowerCase();
+//   let pigArray = [];
+//   for (i=0; i<splitStr.length; i++) {
+//     pigArray.push((splitStr.slice(0,1).match(/[aeiou]/gi) !== null))
+//   }
+//   return pigArray
+// }
+
+//Business Logic
+
+const vowels = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'];
+
+function ruleFinder(word) {
+  if (typeof word !== 'string') {
+    return -1;
   }
-  return pigArray
+
+  word = word.toLowerCase();
+  if (vowels.includes(word[0])) {
+    return 1;
+  }
+
+  if (word.length === 1) {
+    return 2;
+  }
+
+  for (let i = 0; i < word.length; i++) {
+   if (word.length-1 === i) {
+     return 2;
+   }
+   if (vowels.includes(word[i+1])) {
+        if (word[i] === 'q') {
+          if (word[i+1] === 'u') {
+            return 3;
+          } else {
+            return 2;
+          }
+        } else {
+          return 2;
+        }
+    }
+  }
+  return -1;
 }
+
+function vowelWord(word) {
+  if (typeof word !== 'string') {
+    return null;
+  }
+  if (!vowels.includes(word[0])) {
+    return null;
+  }
+  return word.concat("way");
+}
+
+function consonantWord(word) {
+  if (typeof word !== 'string') {
+    return null;
+  }
+  if (vowelWord(word) !== null) {
+    return null;
+  }
+  if (quWord(word) !== null) {
+    return null;
+  }
+  for (let i = 0; i < word.length; i++) {
+    if (vowels.includes(word[i]) || i === word.length - 1) {
+      return (word.slice(i, word.length) + word.slice(0, i) + "ay");
+    }
+  }
+  return null;
+}
+
+function quWord(word) {
+  if (typeof word !== 'string') {
+    return null;
+  }
+  if (vowelWord(word) !== null) {
+    return null;
+  }
+  for (let i = 0; i < word.length; i++) {
+    if (word[i] === 'q' || word[i] === 'Q') {
+      if (word[i+1] === 'u' || word[i+1] === 'U') {
+        return (word.slice(i+2, word.length) + word.slice(0, i+2) + "ay");
+      }
+    }
+  }
+  return null;
+}
+
+function pigLatinTranslator(text) {
+  wordArray = text.split(" ");
+  translatedArray = [];
+  wordArray.forEach(function(word) {
+    let rule = ruleFinder(word);
+    if (rule === -1) {
+      return null;
+    } else if (rule === 1) {
+      translatedArray.push(vowelWord(word));
+    } else if (rule === 2) {
+      translatedArray.push(consonantWord(word));
+    } else if (rule === 3) {
+      translatedArray.push(quWord(word));
+    } else {
+      return null;
+    }
+  });
+  return translatedArray.join(" ");
+}
+
+//UI Logic
+$(document).ready(function() {
+  $("#translate-form").submit(function(event) {
+    event.preventDefault();
+    $("#translated-text").append("<p>" + pigLatinTranslator($("#to-translate").val()) + "</p>");
+  });
+});
